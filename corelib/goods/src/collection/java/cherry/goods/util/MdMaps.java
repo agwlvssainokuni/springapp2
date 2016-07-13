92196168fb55330dce6a3f93e267b0c1d227707e
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2016-07-13 16:45:58 +0900

    SpringApp2：コアライブラリ
    多次元Map形成ユーティリティ。

--- corelib/goods/src/collection/java/cherry/goods/util/MdMaps.java
+++ corelib/goods/src/collection/java/cherry/goods/util/MdMaps.java
@@ -22,117 +22,134 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.TreeMap;
 
+import org.apache.commons.collections4.Factory;
 import org.apache.commons.collections4.Transformer;
 import org.apache.commons.collections4.map.DefaultedMap;
-import org.apache.commons.lang3.tuple.Pair;
-import org.apache.commons.lang3.tuple.Triple;
 
 public class MdMaps {
 
 	@FunctionalInterface
-	public interface Factory {
+	public interface Creator {
 		<K, V> Map<K, V> create();
 	}
 
-	public static final Factory HASHMAP = new Factory() {
+	public static final Creator HASHMAP = new Creator() {
 		@Override
 		public <K, V> Map<K, V> create() {
 			return new HashMap<K, V>();
 		}
 	};
 
-	public static final Factory LINKEDHASHMAP = new Factory() {
+	public static final Creator LINKEDHASHMAP = new Creator() {
 		@Override
 		public <K, V> Map<K, V> create() {
 			return new LinkedHashMap<K, V>();
 		}
 	};
 
-	public static final Factory TREEMAP = new Factory() {
+	public static final Creator TREEMAP = new Creator() {
 		@Override
 		public <K, V> Map<K, V> create() {
 			return new TreeMap<K, V>();
 		}
 	};
 
-	public static <X1, R> Map<X1, R> defaulted1dMap(Factory factory, Transformer<? super X1, ? extends R> transformer) {
-		return DefaultedMap.<X1, R> defaultedMap(factory.<X1, R> create(), x1 -> transformer.transform(x1));
+	public static <X1, R> Map<X1, R> defaulted1dMap(Creator creator, Transformer<? super X1, ? extends R> transformer) {
+		return DefaultedMap.<X1, R> defaultedMap(creator.<X1, R> create(), x1 -> transformer.transform(x1));
 	}
 
-	public static <X1, X2, R> Map<X1, Map<X2, R>> defaulted2dMap(Factory factory,
-			Transformer<Pair<? super X1, ? super X2>, ? extends R> transformer) {
+	public static <X1, X2, R> Map<X1, Map<X2, R>> defaulted2dMap(Creator creator,
+			Transformer<Tuple2<? super X1, ? super X2>, ? extends R> transformer) {
 		return defaulted1dMap(
-				factory,
-				x1 -> DefaultedMap.<X2, R> defaultedMap(factory.<X2, R> create(),
-						x2 -> transformer.transform(Pair.of(x1, x2))));
+				creator,
+				x1 -> DefaultedMap.<X2, R> defaultedMap(creator.<X2, R> create(),
+						x2 -> transformer.transform(Tuples.of(x1, x2))));
 	}
 
-	public static <X1, X2, X3, R> Map<X1, Map<X2, Map<X3, R>>> defaulted3dMap(Factory factory,
-			Transformer<Triple<? super X1, ? super X2, ? super X3>, ? extends R> transformer) {
+	public static <X1, X2, X3, R> Map<X1, Map<X2, Map<X3, R>>> defaulted3dMap(Creator creator,
+			Transformer<Tuple3<? super X1, ? super X2, ? super X3>, ? extends R> transformer) {
 		return defaulted2dMap(
-				factory,
-				p -> DefaultedMap.<X3, R> defaultedMap(factory.<X3, R> create(),
-						x3 -> transformer.transform(Triple.of(p.getLeft(), p.getRight(), x3))));
+				creator,
+				p -> DefaultedMap.<X3, R> defaultedMap(creator.<X3, R> create(),
+						x3 -> transformer.transform(Tuples.of(p.getX1(), p.getX2(), x3))));
 	}
 
-	public static <X1, X2, X3, X4, R> Map<X1, Map<X2, Map<X3, Map<X4, R>>>> defaulted4dMap(Factory factory,
-			Transformer<Pair<Pair<? super X1, ? super X2>, Pair<? super X3, ? super X4>>, ? extends R> transformer) {
+	public static <X1, X2, X3, X4, R> Map<X1, Map<X2, Map<X3, Map<X4, R>>>> defaulted4dMap(Creator creator,
+			Transformer<Tuple4<? super X1, ? super X2, ? super X3, ? super X4>, ? extends R> transformer) {
 		return defaulted3dMap(
-				factory,
-				p -> DefaultedMap.<X4, R> defaultedMap(
-						factory.<X4, R> create(),
-						x4 -> transformer.transform(Pair.of(Pair.of(p.getLeft(), p.getMiddle()),
-								Pair.of(p.getRight(), x4)))));
+				creator,
+				p -> DefaultedMap.<X4, R> defaultedMap(creator.<X4, R> create(),
+						x4 -> transformer.transform(Tuples.of(p.getX1(), p.getX2(), p.getX3(), x4))));
 	}
 
 	public static <X1, X2, X3, X4, X5, R> Map<X1, Map<X2, Map<X3, Map<X4, Map<X5, R>>>>> defaulted5dMap(
-			Factory factory,
-			Transformer<Pair<Triple<? super X1, ? super X2, ? super X3>, Pair<? super X4, ? super X5>>, ? extends R> transformer) {
+			Creator creator,
+			Transformer<Tuple5<? super X1, ? super X2, ? super X3, ? super X4, ? super X5>, ? extends R> transformer) {
 		return defaulted4dMap(
-				factory,
-				p -> DefaultedMap.<X5, R> defaultedMap(
-						factory.<X5, R> create(),
-						x5 -> transformer.transform(Pair.of(
-								Triple.of(p.getLeft().getLeft(), p.getLeft().getRight(), p.getRight().getLeft()),
-								Pair.of(p.getRight().getRight(), x5)))));
+				creator,
+				p -> DefaultedMap.<X5, R> defaultedMap(creator.<X5, R> create(),
+						x5 -> transformer.transform(Tuples.of(p.getX1(), p.getX2(), p.getX3(), p.getX4(), x5))));
 	}
 
 	public static <X1, X2, X3, X4, X5, X6, R> Map<X1, Map<X2, Map<X3, Map<X4, Map<X5, Map<X6, R>>>>>> defaulted6dMap(
-			Factory factory,
-			Transformer<Pair<Triple<? super X1, ? super X2, ? super X3>, Triple<? super X4, ? super X5, ? super X6>>, ? extends R> transformer) {
-		return defaulted5dMap(
-				factory,
-				p -> DefaultedMap.<X6, R> defaultedMap(
-						factory.<X6, R> create(),
-						x6 -> transformer.transform(Pair.of(p.getLeft(),
-								Triple.of(p.getRight().getLeft(), p.getRight().getRight(), x6)))));
+			Creator creator,
+			Transformer<Tuple6<? super X1, ? super X2, ? super X3, ? super X4, ? super X5, ? super X6>, ? extends R> transformer) {
+		return defaulted5dMap(creator, p -> DefaultedMap.<X6, R> defaultedMap(creator.<X6, R> create(),
+				x6 -> transformer.transform(Tuples.of(p.getX1(), p.getX2(), p.getX3(), p.getX4(), p.getX5(), x6))));
 	}
 
-	public static <X1, R> Map<X1, R> defaulted1dMap(Factory factory, R defaultValue) {
-		return defaulted1dMap(factory, p -> defaultValue);
+	public static <X1, R> Map<X1, R> defaulted1dMap(Creator creator, Factory<R> factory) {
+		return defaulted1dMap(creator, p -> factory.create());
 	}
 
-	public static <X1, X2, R> Map<X1, Map<X2, R>> defaulted2dMap(Factory factory, R defaultValue) {
-		return defaulted2dMap(factory, p -> defaultValue);
+	public static <X1, X2, R> Map<X1, Map<X2, R>> defaulted2dMap(Creator creator, Factory<R> factory) {
+		return defaulted2dMap(creator, p -> factory.create());
 	}
 
-	public static <X1, X2, X3, R> Map<X1, Map<X2, Map<X3, R>>> defaulted3dMap(Factory factory, R defaultValue) {
-		return defaulted3dMap(factory, p -> defaultValue);
+	public static <X1, X2, X3, R> Map<X1, Map<X2, Map<X3, R>>> defaulted3dMap(Creator creator, Factory<R> factory) {
+		return defaulted3dMap(creator, p -> factory.create());
 	}
 
-	public static <X1, X2, X3, X4, R> Map<X1, Map<X2, Map<X3, Map<X4, R>>>> defaulted4dMap(Factory factory,
-			R defaultValue) {
-		return defaulted4dMap(factory, p -> defaultValue);
+	public static <X1, X2, X3, X4, R> Map<X1, Map<X2, Map<X3, Map<X4, R>>>> defaulted4dMap(Creator creator,
+			Factory<R> factory) {
+		return defaulted4dMap(creator, p -> factory.create());
 	}
 
 	public static <X1, X2, X3, X4, X5, R> Map<X1, Map<X2, Map<X3, Map<X4, Map<X5, R>>>>> defaulted5dMap(
-			Factory factory, R defaultValue) {
-		return defaulted5dMap(factory, p -> defaultValue);
+			Creator creator, Factory<R> factory) {
+		return defaulted5dMap(creator, p -> factory.create());
 	}
 
 	public static <X1, X2, X3, X4, X5, X6, R> Map<X1, Map<X2, Map<X3, Map<X4, Map<X5, Map<X6, R>>>>>> defaulted6dMap(
-			Factory factory, R defaultValue) {
-		return defaulted6dMap(factory, p -> defaultValue);
+			Creator creator, Factory<R> factory) {
+		return defaulted6dMap(creator, p -> factory.create());
+	}
+
+	public static <X1, R> Map<X1, R> defaulted1dMap(Creator creator, R defaultValue) {
+		return defaulted1dMap(creator, p -> defaultValue);
+	}
+
+	public static <X1, X2, R> Map<X1, Map<X2, R>> defaulted2dMap(Creator creator, R defaultValue) {
+		return defaulted2dMap(creator, p -> defaultValue);
+	}
+
+	public static <X1, X2, X3, R> Map<X1, Map<X2, Map<X3, R>>> defaulted3dMap(Creator creator, R defaultValue) {
+		return defaulted3dMap(creator, p -> defaultValue);
+	}
+
+	public static <X1, X2, X3, X4, R> Map<X1, Map<X2, Map<X3, Map<X4, R>>>> defaulted4dMap(Creator creator,
+			R defaultValue) {
+		return defaulted4dMap(creator, p -> defaultValue);
+	}
+
+	public static <X1, X2, X3, X4, X5, R> Map<X1, Map<X2, Map<X3, Map<X4, Map<X5, R>>>>> defaulted5dMap(
+			Creator creator, R defaultValue) {
+		return defaulted5dMap(creator, p -> defaultValue);
+	}
+
+	public static <X1, X2, X3, X4, X5, X6, R> Map<X1, Map<X2, Map<X3, Map<X4, Map<X5, Map<X6, R>>>>>> defaulted6dMap(
+			Creator creator, R defaultValue) {
+		return defaulted6dMap(creator, p -> defaultValue);
 	}
 
 	public static <X1, R> Optional<R> getOpt(Map<X1, R> map, X1 x1) {
